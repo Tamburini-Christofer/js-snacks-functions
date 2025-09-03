95b99a34eedadd255b679dbf32a03835d92c1816
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-03 13:37:50 +0200

    Esercizio 3 completato

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,21 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca 
+un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+let soloLetteraA = (value) => {    
+    let inizialeA = [];
+    for (let i = 0; i < value.length; i++) {
+        if (value[i][0] === "A") {
+         inizialeA.push(value[i]);
+        }
+    }
+    console.log(inizialeA);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+soloLetteraA (names);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
