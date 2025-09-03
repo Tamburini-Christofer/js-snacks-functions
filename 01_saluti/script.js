12d86d4d0295b38c76485dd2b162fd31f4040841
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-03 13:13:24 +0200

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,14 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+// Dichiara la funzione qui.
+let saluto = (userName) => {
+    console.log(`Ciao ${userName}`)
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+saluto (userName);
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
