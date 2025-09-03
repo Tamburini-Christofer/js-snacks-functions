c6b95c3a1093c48ff70e4630b6ac4db944e656e1
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-03 13:30:31 +0200

    Esercizi 2  completato

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,19 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
-
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+let soloIniziali = (value) => {    
+    let iniziali = [];
+    for (let i = 0; i < value.length; i++) {
+            iniziali.push(value[i][0]);
+        } 
+    console.log(iniziali);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+soloIniziali (names);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+//Esercizio fatto
